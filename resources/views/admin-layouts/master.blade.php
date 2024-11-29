494782bcd531e8c3799aff343a6caa41ef2441f1
Dean12 <mahyudinakbar10@gmail.com>
2024-11-29 09:40:27 +0700

    first commit

---
+++ resources/views/admin-layouts/master.blade.php
@@ -0,0 +1,10 @@
+@include('admin-layouts.header')
+<!-- Left side column. contains the logo and sidebar -->
+@include('admin-layouts.sidebar')
+
+
+@yield('content')
+
+
+<!-- /.content-wrapper -->
+@include('admin-layouts.footer')
